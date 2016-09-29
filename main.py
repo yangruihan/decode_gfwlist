aba4d11013fc8b9d7fbfa571af285ad16832de79
C.y <yangruihan@vip.qq.com>
2016-09-30 00:57:07 +0800

    Init

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+
+import requests
+import re
+
+GFWLIST_CONTENT_URL = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
+KEY_STR = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+SAVE_PATH = '/Users/yangruihan/.ShadowsocksX/user-rule.txt'
+
+def decode64(input_str):
+    """
+    解析GFWList算法
+    """
+    input_str = re.sub(r'[^A-Za-z0-9\+\/\=]', "", input_str)
+    i = 0
+    output_str = ''
+    while i < len(input_str):
+        enc1 = KEY_STR.index(input_str[i])
+        i += 1
+        enc2 = KEY_STR.index(input_str[i])
+        i += 1
+        enc3 = KEY_STR.index(input_str[i])
+        i += 1
+        enc4 = KEY_STR.index(input_str[i])
+        i += 1
+
+        chr1 = (enc1 << 2) | (enc2 >> 4)
+        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2)
+        chr3 = ((enc3 & 3) << 6) | enc4
+
+        output_str += chr(chr1)
+
+        if enc3 != 64:
+            output_str += chr(chr2)
+
+        if enc4 != 64:
+            output_str += chr(chr3)
+
+    return output_str
+
+if __name__ == '__main__':
+    raw_content = requests.get(GFWLIST_CONTENT_URL).text
+    temp_str = decode64(raw_content)
+
+    with open(SAVE_PATH, 'w') as file:
+        file.write(temp_str)
